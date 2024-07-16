9127172333a793c6f9c7a1c98411b8e22797c969
nglsena0722c <nglsena0722@com2us.com>
2024-07-16 10:04:54 +0900

    Dev:  Add dev env

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -14,7 +14,9 @@ const Chat: React.FC = () => {
   const [state, setState] = useState<{ message: string; }>({ message: '' });
   const [chat, setChat] = useState<ChatMessage[]>([]);
 
-  const socket = io(process.env.REACT_APP_API_URL || 'http://localhost:5641/');
+  const socket = io(process.env.REACT_APP_ENV !== "development" ? process.env.REACT_APP_API_URL || '' : 'http://localhost:5641/',  process.env.REACT_APP_ENV !== "development" ? {
+    path : '/discord/socket.io'
+  } : undefined);
   const { status, wallets } = useWallet();
 
   useEffect(() => {
