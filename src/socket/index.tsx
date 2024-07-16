61e428348eaa384a4b88e5d1dde22f035b5b5742
nglsena0722c <nglsena0722@com2us.com>
2024-07-16 13:30:57 +0900

    Fix : Add socket connection setting

--- src/socket/index.tsx
+++ src/socket/index.tsx
@@ -1,5 +1,7 @@
 import { io } from 'socket.io-client';
 
-export const socket = io(process.env.REACT_APP_ENV !== "development" ? process.env.REACT_APP_API_URL || '' : 'http://localhost:5641/',  process.env.REACT_APP_ENV !== "development" ? {
-  path : '/discord/socket.io'
-} : undefined);
+export const socket = io(process.env.REACT_APP_ENV !== "development" ? process.env.REACT_APP_API_URL || '' : 'http://localhost:5641/', {
+  ...(process.env.REACT_APP_ENV !== "development" && { path: '/discord/socket.io' }),
+  reconnectionAttempts: 5, // 최대 재시도 횟수
+  reconnectionDelay: 12000, // 각 재시도 간의 지연 시간 (12초)
+});
