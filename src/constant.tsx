dcee1db3d9ddcd1bc44f8a9262cdb309e387d76d
nglsena0722c <nglsena0722@com2us.com>
2024-07-25 18:02:26 +0900

    Fix : Fix contract address

--- src/constant.tsx
+++ src/constant.tsx
@@ -1,4 +1,8 @@
 export const plawarContractOwner = "xpla1k5f4rg0amatncw8vdxq3dqr2q27fdfqs2j5l62";
 export const claimContractOwner = "xpla1hl7v8n7mapkwyt5kfzvf6fqxprxlmgfwrfjell";
-export const plawarContractAddress = "xpla1w3rgmzg4kj2q8s2zyc4qdh7xxkru9uvwr9tesv90sq25q0ynppkqz0j6t3";
-export const claimContractAddress = "xpla1rspt37vuhvnjmlw8vm87ew73thhw5skcytgyz5sm85x69kaht59sv37kj7";
+// test warmin and trucemin is 1
+// export const plawarContractAddress = "xpla1w3rgmzg4kj2q8s2zyc4qdh7xxkru9uvwr9tesv90sq25q0ynppkqz0j6t3";
+// export const claimContractAddress = "xpla1rspt37vuhvnjmlw8vm87ew73thhw5skcytgyz5sm85x69kaht59sv37kj7";
+
+export const plawarContractAddress = "xpla1v4g8lsa5vuuk9h60akxg6dc24ewx4cajuesat2yyv2rsjzgu076s0gg5ap";
+export const claimContractAddress = "xpla1mzt7c8pe0t9c45ee3etqz683xewfllw8grr7wflesqmntm65k28s26mxt9";
